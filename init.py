9cd17d21f32fca5eef2ad2b0d1ea546473f62a36
платон <platonshugalev870@gmail.com>
2026-07-20 20:42:08 +0300

    Delete init.py

--- init.py
+++
@@ -1,6 +0,0 @@
-from .main_window import GodOSWindow
-from .god_terminal import GodTerminal
-from .prophecy_viewer import ProphecyViewer
-from .prayer_panel import PrayerPanel
-from .revelation_display import RevelationDisplay
-from .sacred_visualizer import SacredVisualizer
